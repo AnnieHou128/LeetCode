8abbc80ab6d8fa51e39f92353056876fd1ea63fa
AnnieHou <88053892+AnnieHou128@users.noreply.github.com>
2023-09-13 00:30:38 -0400

    Time: 1 ms (100%), Space: 44.8 MB (70.64%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -5,15 +5,12 @@ class Solution {
         int indexStart = 0;
         int i = nums.length - 1;
         while (indexStart <= indexBack){
-                System.out.println(i);
                 if (Math.abs(nums[indexStart]) > Math.abs(nums[indexBack])){
                     arr[i] = nums[indexStart] * nums[indexStart];
-                    System.out.println("start:"+indexStart);
                     indexStart += 1;
                     i--;
                 } else {
                     arr[i] = nums[indexBack] * nums[indexBack];
-                    System.out.println("b:"+indexBack);
                     indexBack -= 1;
                     i--;
                 }
